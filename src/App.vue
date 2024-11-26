38cb2bf74088699d77a93e27e3866bb55896610a
chaerimjo-web <chaerim010710@gmail.com>
2024-11-26 16:49:03 +0900

    체크 여부 반영하기

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,41 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import TodoItem from './components/TodoItem.vue';
+import { reactive, ref } from 'vue';
+import Todo from './components/TodoItem.vue';
+
+const todoText = ref('');
+
+const addtodo = (e)=>{ //엔터
+  todoText.value = e.target.value;
+  const todoTextLength =  todoText.value.trim().length;
+  //빈 칸 없애기 -> 대상.trim()
+  if(todoTextLength > 0 ){ //값이 있다면 length > 0
+    todos.push({id:todoTextLength+1, title:todoText.value, checked:false})
+  } //todos에 값을 넣음
+  todoText.value = ''; //비우기
+}
+const todos = reactive([
+  {id:1, title:'웹 배우기', checked:false}, //체크여부
+  {id:2, title:'취직하기', checked:false}
+])
+const toggleCheckbox = (id, checked)=>{ //일치여부 대상.findIndex(각요소=>{return 조건}) 하나만 있으면 리턴 제거
+  const idx = todos.findIndex(todo=> todo.id === id); //넘어온 아이디/ 투두의 아이디가 일치하면
+  todos[idx].checked = checked
+  console.log(id, checked, todos.values);
+}
 
 </script>
 
 <template>
   <div id="app" class="container">
     <h1>Vue Todo App</h1>
-    <BFormInput placeholder="할 일을 입력하세요" />
-    <TodoItem todo="맛난거 먹기!!!"/>
+    <BFormInput placeholder="할 일을 입력하세요" @keyup.enter="addtodo" v-model="todoText"/>
+    <!-- v-model="todoText" -> 사용자가 입력한 값 value역할 -->
+    <Todo
+    v-for="todo in todos"
+    :key="todo.id"
+    :todo="todo"
+    @toggle-checkbox="toggleCheckbox"
+    />
   </div>
 </template>
 
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,19 @@
 <script setup>
-defineProps({
+const props = defineProps({
   todo: {
     type: String,
     required: true,
   },
 })
+
+const emit = defineEmits(['toggle-checkbox'])
+const toggleCheckbox = (e)=>{
+
+  emit('toggle-checkbox', {
+    id: props.todo.id,
+    checked:e.target.checked
+  }) //함수 실행 | true false 여부가 넘어가야함 인풋에 변동사항 생기면 @change="toggleCheckbox"
+}
 </script>
 
 <template>
@@ -15,11 +24,15 @@ defineProps({
       name="checkbox-1"
       value="accepted"
       unchecked-value="not_accepted"
+      @change="toggleCheckbox"
     >
-      {{todo}}
+     <span :class="todo.checked && 'muted'"> {{todo.title}}</span>
     </BFormCheckbox>
   </div>
 </template>
-
+<!-- //todo check의 값이true면 muted 클래스명을 추가 (밑줄) -->
 <style scoped>
+.muted{
+  text-decoration: line-through;
+}
 </style>
