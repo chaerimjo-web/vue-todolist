794bc6406105c515b52cb754c230e93bdf613051
chaerimjo-web <chaerim010710@gmail.com>
2024-11-27 10:18:29 +0900

    todo 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,55 @@
 <script setup>
 import { reactive, ref } from 'vue';
 import Todo from './components/TodoItem.vue';
-
 const todoText = ref('');
 
-const addtodo = (e)=>{ //엔터
+const addtodo = (e)=>{
   todoText.value = e.target.value;
-  const todoTextLength =  todoText.value.trim().length;
-  //빈 칸 없애기 -> 대상.trim()
-  if(todoTextLength > 0 ){ //값이 있다면 length > 0
-    todos.push({id:todoTextLength+1, title:todoText.value, checked:false})
-  } //todos에 값을 넣음
-  todoText.value = ''; //비우기
+  const todoTextLength = todoText.value.trim().length;   //빈칸 없애기. 대상.trim()
+  if(todoTextLength > 0){
+    todos.push({id:todoTextLength+1,title:todoText.value,checked:false})
+  }
+  todoText.value = '';
 }
 const todos = reactive([
-  {id:1, title:'웹 배우기', checked:false}, //체크여부
-  {id:2, title:'취직하기', checked:false}
+  {id:1,title:'웹 배우기',checked:false},
+  {id:2,title:'취직하기',checked:false}
 ])
-const toggleCheckbox = (id, checked)=>{ //일치여부 대상.findIndex(각요소=>{return 조건}) 하나만 있으면 리턴 제거
-  const idx = todos.findIndex(todo=> todo.id === id); //넘어온 아이디/ 투두의 아이디가 일치하면
-  todos[idx].checked = checked
-  console.log(id, checked, todos.values);
+
+const toggleCheckbox = ({id, checked})=>{
+  const idx = todos.findIndex(todo=>todo.id === id);
+  todos[idx].checked = checked;
+}
+
+const todoDelete = ({id})=>{
+  if(window.confirm('정말 삭제할까요?')){
+    const idx = todos.findIndex(todo=>todo.id === id);
+    todos.splice(idx,1); //배열안의 값을 하나만 지우기
+  }
 }
 
 </script>
 
 <template>
-  <div id="app" class="container">
-    <h1>Vue Todo App</h1>
-    <BFormInput placeholder="할 일을 입력하세요" @keyup.enter="addtodo" v-model="todoText"/>
-    <!-- v-model="todoText" -> 사용자가 입력한 값 value역할 -->
-    <Todo
+ <div id="app" class="container">
+  <h1>Vue Todo App</h1>
+  <!--
+  입력 내용이 실시간 반영
+  <BFormInput placeholder="할일을 입력하세요"  v-model="todoText"/>
+  <Todo :todo="todoText"/>
+  -->
+  <!-- 입력후 엔터시 내용이 추가 -->
+  <BFormInput placeholder="할일을 입력하세요"  @keyup.enter="addtodo" v-model="todoText"/>
+  <Todo
     v-for="todo in todos"
     :key="todo.id"
     :todo="todo"
     @toggle-checkbox="toggleCheckbox"
-    />
-  </div>
+    @todo-delete="todoDelete"
+  />
+
+ </div>
 </template>
 
 <style scoped>
-
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,12 +1,17 @@
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps({
   todo: {
-    type: String,
+    type: Object,
     required: true,
   },
 })
 
-const emit = defineEmits(['toggle-checkbox'])
+const emit = defineEmits(['toggle-checkbox'],['todo-delete'])
+
+const isChecked = ref(props.todo.checked);
+
 const toggleCheckbox = (e)=>{
 
   emit('toggle-checkbox', {
@@ -14,20 +19,26 @@ const toggleCheckbox = (e)=>{
     checked:e.target.checked
   }) //함수 실행 | true false 여부가 넘어가야함 인풋에 변동사항 생기면 @change="toggleCheckbox"
 }
+const todoDelete = ()=>{
+
+  emit('todo-delete', {
+    id: props.todo.id,
+  })
+}
 </script>
 
 <template>
-  <div >
+  <div class="d-flex gap-3 align-items-center mt-3">
     <BFormCheckbox
-      id="checkbox-1"
-      v-model="status"
-      name="checkbox-1"
+      :id="`{checkbox-${todo.id}}`"
+      v-model="isChecked"
+      :name="`{checkbox-${todo.id}}`"
       value="accepted"
-      unchecked-value="not_accepted"
       @change="toggleCheckbox"
     >
-     <span :class="todo.checked && 'muted'"> {{todo.title}}</span>
+     <span :class="{muted: todo.checked}"> {{todo.title}}</span>
     </BFormCheckbox>
+    <BButton variant="danger" size="sm" @click="todoDelete">삭제</BButton>
   </div>
 </template>
 <!-- //todo check의 값이true면 muted 클래스명을 추가 (밑줄) -->
